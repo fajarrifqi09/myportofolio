e8d72aec1d2bf1ac2fb66702523a1add6ad4149f
Fajar Rifqi Bagaskara <130422176+fajarrifqi09@users.noreply.github.com>
2024-03-01 21:43:01 +0700

    Portofolio Fajar
    Sebuah Web Portfolio yang Responsive.

---
+++ main.js
@@ -0,0 +1,158 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+    } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+    }
+  }
+
+
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Design UI/UX","Web Developer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+function sendEmail() {
+  // Alamat email yang ingin dituju
+  var toEmail = "fajarrifqi97@email.com";
+  
+  // Subjek email
+  var subject = "Kirim Pesan";
+  
+  // Isi pesan email
+  var body = "Isi pesan email.";
+
+  // Membuat URL untuk membuka aplikasi email default dengan informasi yang diberikan
+  var mailtoLink = "mailto:" + toEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+  // Membuka aplikasi email default pengguna
+  window.location.href = mailtoLink;
+}
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+// Fungsi untuk memperbesar gambar
+function enlargeImage(box) {
+  var img = box.querySelector('img');
+  var enlargedImage = document.getElementById("enlarged-image");
+  enlargedImage.src = img.src;
+
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+
+  // Menambahkan class 'enlarged' untuk animasi
+  box.classList.add("enlarged");
+}
+
+// Fungsi untuk menutup overlay
+function closeOverlay() {
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "none";
+
+  // Menghapus class 'enlarged' untuk mengembalikan ukuran gambar
+  var boxes = document.querySelectorAll('.project-box');
+  boxes.forEach(function(box) {
+      box.classList.remove("enlarged");
+  });
+}
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+    }  else {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
